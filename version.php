dab45119a30342fc301971ddabfc12e12151190f
Michael Meneses <michael.douglas.meneses.2@gmail.com>
2017-04-05 11:51:39 -0300

    Build version 0.2

--- version.php
+++ version.php
@@ -8,8 +8,8 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$plugin->version = 2017032900;   // The (date) version of this plugin
+$plugin->version = 2017040500;   // The (date) version of this plugin
 $plugin->requires = 2016112900; // Requires this Moodle version
 $plugin->component = 'local_tematica';  // To check on upgrade, that module sits in correct place
-$plugin->release = "0.1"; // Human readable version name
-$plugin->maturity = MATURITY_ALPHA; //MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC, MATURITY_STABLE
+$plugin->release = "0.2"; // Human readable version name
+$plugin->maturity = MATURITY_BETA; //MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC, MATURITY_STABLE
